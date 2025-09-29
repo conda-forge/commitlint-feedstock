bde09de59a51a4818880e3a5d074fba8adb57d5d
Daniel Nachun <daniel.nachun@gmail.com>
2025-09-29 00:54:06 -0700

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 # Create package archive and install globally
 npm pack --ignore-scripts
 npm install -ddd \
+    --no-bin-links \
     --global \
     --build-from-source \
     ${PKG_NAME}-cli-${PKG_VERSION}.tgz
@@ -17,7 +18,14 @@ jq 'del(.devDependencies)' package.json.bak > package.json
 pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
+mkdir -p ${PREFIX}/bin
+tee ${PREFIX}/bin/commitlint << EOF
+#!/bin/sh
+exec \${CONDA_PREFIX}/lib/node_modules/@commitlint/cli/cli.js "\$@"
+EOF
+chmod +x ${PREFIX}/bin/commitlint
+
 # Create batch wrapper
 tee ${PREFIX}/bin/commitlint.cmd << EOF
-call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\bin\commitlint %*
+call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\lib\node_modules\@commitlint\cli\cli.js %*
 EOF
